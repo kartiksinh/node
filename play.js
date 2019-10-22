b167cb266cd31ab97bd878de587e00be4d46337e
kartik <kartiksinh24@gmail.com>
2019-10-22 18:38:06 +0530

    javascript nex-gen functions

---
+++ play.js
@@ -0,0 +1,13 @@
+var name = "Kartik";
+var age = 26;
+var hasHobbies = true;
+
+function summarizeUser(userName, uerAge, userHobbies){
+    return (
+    'Name is ' + userName + 
+    ' age is ' + age + 
+    ' hobbies has :' + hasHobbies
+    );
+}
+
+console.log(summarizeUser(name, age, hasHobbies)); 
