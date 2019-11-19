f0146845c1546aabe7e19c2e6a9b8605770499b1
kartik <kartiksinh24@gmail.com>
2019-11-19 22:01:13 +0530

    adding routes

--- app.js
+++ app.js
@@ -1,44 +1,6 @@
 const http = require('http');
-const fs = require('fs');
-
-const server = http.createServer((req, res) => {
-
-    const url = req.url;
-    const method = req.method;
-
-    if (url === '/') {
-        res.write('<html>');
-        res.write('<head><title>Node App</title></head>')
-        res.write('<body>');
-        res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></from>')
-        res.write('</body>');
-        res.write('</html>');
-        return res.end();
-    }
-    if (url === '/message' && method === 'POST') {
-        const body = [];
-        req.on('data', (chunk) => {
-            body.push(chunk);
-        });
-        return req.on('end', () => {
-            const parseBody = Buffer.concat(body).toString();
-            const message = parseBody.split('=')[1];
-            fs.writeFile('message.text', message, (err) => {
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                return res.end();
-            });
-           
-        });
-    }
-
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>Node App</title></head>')
-    res.write('<body><h1>Hello from NOde JS</h1></body>')
-    res.write('</html>');
-    res.end();
-    // process.exit();
-});
+const routes = require('./routes');
+console.log(routes.text);
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
