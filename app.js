7d4dac28a859634fac3013f700c83d9856fa5de5
kartik <kartiksinh24@gmail.com>
2019-12-23 15:53:27 +0530

    installed expressJS

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-const http = require('http');
-const routes = require('./routes');
-console.log(routes.text);
-const server = http.createServer(routes.handler);
 
-server.listen(3000);
+const express = require('express');
+
+const app = express();
+
+app.use('/', (req,res, next)=> {
+    console.log("this always runs");
+    next();
+});
+app.use('/add-product',(req, res, next) => {
+    res.send('<h1>ADD Product Page</h1>');
+});
+app.use('/',(req, res, next) => {
+    console.log("I am in another middleware");
+    res.send('<h1>Hello from Express CSS kartik</h1>');
+});
+
+app.listen(3000)
