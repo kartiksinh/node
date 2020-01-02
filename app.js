0b7884547fbe88db4f1eace9171152fb467931da
kartik <kartiksinh24@gmail.com>
2020-01-02 17:14:29 +0530

    adding routes to express JS

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 
 const express = require('express');
-
 const app = express();
+const bodyParser = require('body-parser');
 
-app.use('/', (req,res, next)=> {
-    console.log("this always runs");
-    next();
-});
-app.use('/add-product',(req, res, next) => {
-    res.send('<h1>ADD Product Page</h1>');
-});
-app.use('/',(req, res, next) => {
-    console.log("I am in another middleware");
-    res.send('<h1>Hello from Express CSS kartik</h1>');
-});
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.listen(3000)
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(shopRoutes);
+app.use(adminRoutes);
+
+
+app.listen(3000);
