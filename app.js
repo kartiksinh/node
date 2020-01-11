def0f687513a03a8131e83c2d1246b90ae9ed322
Rutvij Kanzaria <rutvijkanzaria@Rutvijs-MacBook-Pro.local>
2020-01-12 01:23:55 +0530

    Filtering paths

--- app.js
+++ app.js
@@ -9,7 +9,10 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(shopRoutes);
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
+});
 
 
 app.listen(3000);
