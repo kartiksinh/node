b32a16ffe1f1f2135719227548e2825f0e733e4f
Rutvij Kanzaria <rutvijkanzaria@Rutvijs-MacBook-Pro.local>
2020-01-12 11:45:03 +0530

    adding views

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
+const path = require('path');
+
 const router = express.Router();
 
 router.get('/',(req, res, next) => {
-    res.send('<h1>Hello from kartik</h1>');
+    res.sendFile()
 });
 
 module.exports = router;
